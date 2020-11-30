722a7e5dddc9859e3fd3a2b4e5907357ce531f27
shinyay <shinya.com@gmail.com>
2020-11-30 20:58:43 +0900

    Add Docket

--- src/main/kotlin/com/google/shinyay/configuration/SwaggerConfiguration.kt
+++ src/main/kotlin/com/google/shinyay/configuration/SwaggerConfiguration.kt
@@ -1,12 +1,21 @@
 package com.google.shinyay.configuration
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.service.ApiInfo
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
 
 @Configuration
 class SwaggerConfiguration {
 
     private fun apiInfo(): ApiInfo = ApiInfoBuilder()
+            .build()
+
+    @Bean
+    fun api(): Docket = Docket(DocumentationType.SWAGGER_2)
+            .apiInfo(apiInfo())
+            .select()
             .build()
 }
