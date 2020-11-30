3baaf82379dd3ee160da1662c3499d733d946f89
shinyay <shinya.com@gmail.com>
2020-11-30 20:49:10 +0900

    Add ApiInfoBuilder

--- src/main/kotlin/com/google/shinyay/configuration/SwaggerConfiguration.kt
+++ src/main/kotlin/com/google/shinyay/configuration/SwaggerConfiguration.kt
@@ -1,4 +1,12 @@
 package com.google.shinyay.configuration
 
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.service.ApiInfo
+
+@Configuration
 class SwaggerConfiguration {
+
+    private fun apiInfo(): ApiInfo = ApiInfoBuilder()
+            .build()
 }
