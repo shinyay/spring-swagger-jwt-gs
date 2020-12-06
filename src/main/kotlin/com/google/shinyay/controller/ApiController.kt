2cb7a1bcab1c8ca30eda64007f71dc4913e1a1da
shinyay <shinya.com@gmail.com>
2020-12-06 21:20:44 +0900

    Add Api Definition annotation

--- src/main/kotlin/com/google/shinyay/controller/ApiController.kt
+++ src/main/kotlin/com/google/shinyay/controller/ApiController.kt
@@ -1,7 +1,9 @@
 package com.google.shinyay.controller
 
+import io.swagger.annotations.Api
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@Api(tags = ["Hello", "Default"], description = "Default Controller")
 class ApiController {
 }
