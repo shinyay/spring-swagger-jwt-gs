e33399d3da0498d8d4bdfeb1567a003961c6e341
shinyay <shinya.com@gmail.com>
2020-12-19 22:18:28 +0900

    Add return curren time

--- src/main/kotlin/com/google/shinyay/controller/ApiController.kt
+++ src/main/kotlin/com/google/shinyay/controller/ApiController.kt
@@ -2,8 +2,16 @@ package com.google.shinyay.controller
 
 import io.swagger.annotations.Api
 import org.springframework.web.bind.annotation.RestController
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 @RestController
 @Api(tags = ["Hello", "Default"], description = "Default Controller")
 class ApiController {
+
+    fun hello() = listOf<String>(
+            "Hello",
+            ZonedDateTime.now(ZoneId.of("Japan")).
+            format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
 }
