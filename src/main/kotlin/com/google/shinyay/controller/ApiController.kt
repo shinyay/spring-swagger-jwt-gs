e709c13b79a0dd301b1cd225f7ae8ff2bd088804
shinyay <shinya.com@gmail.com>
2020-12-30 21:52:36 +0900

    Add GetMapping Annotation

--- src/main/kotlin/com/google/shinyay/controller/ApiController.kt
+++ src/main/kotlin/com/google/shinyay/controller/ApiController.kt
@@ -2,6 +2,7 @@ package com.google.shinyay.controller
 
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import java.time.ZoneId
 import java.time.ZonedDateTime
@@ -13,6 +14,7 @@ class ApiController {
 
     @ApiOperation(value = "This function is to get Hello Response",
                   notes = "This function is to get Hello and Current time as Response")
+    @GetMapping()
     fun hello() = listOf<String>(
             "Hello",
             ZonedDateTime.now(ZoneId.of("Japan")).
